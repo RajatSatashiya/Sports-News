a7b921a3f0350c8e66f682349e081feb64b407de
Rajat Satashiya <kshri4398@gmail.com>
2023-02-08 19:11:16 +0530

    driver standings

--- src/Components/F1/F1.jsx
+++ src/Components/F1/F1.jsx
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useSearchParams } from "react-router-dom";
 import "../Stylings/F1.css";
+import { standings2022, rounds2022 } from "./Resource/F1Resource";
 
 function F1() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [apidata, setApidata] = useState([]);
+  // const [apidata, setApidata] = useState(rounds2022);
+  const [standings, setStandings] = useState([]);
   const [year, setYear] = useState(
     searchParams.get("year")
       ? searchParams.get("year")
@@ -27,6 +30,35 @@ function F1() {
       console.log("Error: " + e.message);
     }
   };
+
+  const getStandings = async (val) => {
+    try {
+      const response = await fetch(
+        `http://ergast.com/api/f1/${val}/driverStandings.json`
+      );
+      const data = await response.json();
+      console.log(data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
+      setStandings(
+        data.MRData.StandingsTable.StandingsLists[0].DriverStandings
+      );
+    } catch (e) {
+      console.log("Error: " + e);
+    }
+  };
+
+  const displayDriversStandings = standings.map((item, index) => {
+    return (
+      <tr key={index}>
+        <td>{item.positionText}</td>
+        <td>
+          {item.Driver.givenName} {item.Driver.familyName}
+        </td>
+        <td>{item.Constructors[0] && item.Constructors[0].name}</td>
+        <td>{item.points}</td>
+        <td>{item.wins}</td>
+      </tr>
+    );
+  });
 
   const displayResult = apidata.map((item, index) => {
     return (
@@ -52,13 +84,17 @@ function F1() {
         ? searchParams.get("year")
         : new Date().getFullYear()
     );
+    getStandings(
+      searchParams.get("year")
+        ? searchParams.get("year")
+        : new Date().getFullYear()
+    );
   }, []);
 
   return (
     <>
       <div className="calendar">
         <h1>F1 Calendar - {year}</h1>
-
         <select
           onChange={(e) => {
             const theseason = e.target.value;
@@ -71,7 +107,24 @@ function F1() {
           {displayDropdown}
         </select>
 
-        <div className="grandPrix">{displayResult}</div>
+        <div className="f1Standings-container">
+          <div className="grandPrix">{displayResult}</div>
+
+          <div className="standingTable f1Table">
+            <table>
+              <thead>
+                <tr>
+                  <th>Pos</th>
+                  <th>Driver</th>
+                  <th>Constructor</th>
+                  <th>Pts.</th>
+                  <th>Wins</th>
+                </tr>
+              </thead>
+              <tbody>{displayDriversStandings}</tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </>
   );
